d156d99196ad27a9f5b1cc4aefe0d6d5ad0794fb
lucas <lucas_developer@163.com>
2022-04-28 09:18:37 +0800

    线程

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -20,14 +20,24 @@ void callback(int count,const char* threadName){
     }
 }
 
+void callback2(int count,const char* threadName){
+    for(int i=count;i>0;i--){
+        //互斥锁
+        lock_guard<mutex> guard(lock);
+        LOGD("执行线程%s:%d",threadName,value)
+        usleep(1000);
+        value--;
+    }
+}
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_lucas_androidcpp_jni_JniThread_createJniThread(JNIEnv *env, jobject thiz) {
     //创建线程
     int count =100;
     LOGD("创建线程")
-    thread t = thread(callback, count,"thread1");
-    thread t2 = thread(callback, count,"thread2");
+    thread t = thread(callback2, count,"thread1");
+    thread t2 = thread(callback2, count,"thread2");
     LOGD("创建完成")
     t.join();
     LOGD("------")
